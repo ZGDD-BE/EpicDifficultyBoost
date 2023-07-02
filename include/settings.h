e5c7b3ac7e5cc8bd33927b02ba07b611f48b4bc3
dofes <91889957+dofes@users.noreply.github.com>
2023-07-02 19:17:52 +0800

    style: code formatting and cleanup

--- include/settings.h
+++ include/settings.h
@@ -4,24 +4,34 @@
 #include <utils/SU.hpp>
 
 namespace settings {
-    using jsonDictVector = std::vector<std::unordered_map<std::string, nlohmann::json>>;
-    extern std::unordered_map<std::string,int> Foods;
-    extern SU<bool> FoodEnhance;
-    extern SU<bool> SuperCreeper;
+    // HardCorePlayer settings
     extern SU<bool> HardCorePlayer;
+    extern SU<bool> FoodEnhance;
+    extern SU<bool> NormalHealth;
+    extern SU<int> NormalHealthPoint;
+    extern SU<jsonDictVector> FoodList;
+    extern SU<bool> KeepInv;
+    extern SU<bool> SleepingHungerToHealth;
+    extern SU<std::string> KeepInvCostItem;
+    extern std::unordered_map<std::string, int> Foods;
+
+    // FishingMonster settings
     extern SU<bool> FishingMonster;
     extern SU<double> FishingMonsterPersentage;
     extern SU<jsonDictVector> FishingMonsterTypeList;
     extern std::vector<double> FishingMonsterchances;
+
+    // SuperCreeper settings
+    extern SU<bool> SuperCreeper;
+
+    // SuperSkeleton settings
     extern SU<bool> SuperSkeleton;
     extern SU<int> SuperSkeletonSpeedLimit;
-    extern SU<bool> NormalHealth;
-    extern SU<int> NormalHealthPoint;
+
+    // GameRules settings
     extern SU<bool> AlwaysNight;
     extern SU<bool> NaturalRegeneration;
-    extern SU<bool> KeepInv;
-    extern SU<std::string> KeepInvCostItem;
-    extern SU<bool> SleepingHungerToHealth;
+
     nlohmann::json globaljson();
     void initjson(nlohmann::json json);
     void writeDefaultConfig(const std::string& fileName);
